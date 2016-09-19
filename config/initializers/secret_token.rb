2b264bc9e65364b417fc68811dab306066aa3ee0
ijones16 <ian@egghead.io>
2016-09-19 20:45:05 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CatsQuiz::Application.config.secret_key_base = '487747354e534ddff6501b0df56274d053e17748600cafaa123e7763d89ab7e937a27262d7de9109e95e1e2980b7e7ea2508b3e90ae4e709ee17911e002f01ab'
